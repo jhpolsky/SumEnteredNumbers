6e61163a500e3efdd3338762b4ba7dfdecc8769d
Joe Polsky <jpolsky@allvuesystems.com>
2021-10-13 11:49:30 -0400

    intial commit

---
+++ SumEnteredNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace SumEnteredNumbers
+{
+	class SumEnteredNumbers
+	{
+
+		static void Main( string[] args )
+		{
+			var sum = 0;
+			while( true )
+			{
+				Console.WriteLine("Enter Number. Type \"ok\" to exit");
+				var input = Console.ReadLine();
+
+				if( input.ToLower() == "ok" )
+					break;
+
+				sum += Convert.ToInt32(input);
+
+			}
+
+			Console.WriteLine( "Sum of entered numbers is: " + sum );
+
+		}
+	}
+}
